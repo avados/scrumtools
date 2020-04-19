de15cd9446bd972a34c9f4f7d36b25979361a910
C L <C L@AVADOS-LENOVO>
2020-04-19 17:54:47 -0400

    Added coverage

--- scrumtools/settings.py
+++ scrumtools/settings.py
@@ -21,13 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'your secret key'
 
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
 try:
     from local_settings import *
 except ImportError:
     pass
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
 
